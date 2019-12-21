1187802e095b2440789c03165a3236ec9fc03418
WillowWisp <37970784+WillowWisp@users.noreply.github.com>
2019-12-21 10:39:53 +0700

    Add CheckboxGroup
    Refactor Checkbox stuff

--- src/layout/pages/PageRoom/components/DialogAddOrEditRoom.tsx
+++ src/layout/pages/PageRoom/components/DialogAddOrEditRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent, useEffect } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 
 // Misc
 import * as roomAPI from '../../../../api/roomAPI';
@@ -9,17 +9,18 @@ import { ScreenType } from '../../../../interfaces/screenType';
 
 // Component
 import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormLabel from '@material-ui/core/FormLabel';
 import TextField from '@material-ui/core/TextField';
+
+// Custom Component
+import CheckboxGroup from '../../../../components/CheckboxGroup';
 
 interface IOptionScreenType {
   screenType: ScreenType,
@@ -38,23 +39,6 @@ const DialogAddOrEditRoom: FunctionComponent<IDialogAddOrEditRoomProps> = (props
   const [roomInput, setRoomInput] = useState<RoomInput>({ name: '', clusterId: '5dc77c3f06e6b13dc44b6cac', screenTypeIds: [], totalRows: 0, totalSeatsPerRow: 0 });
   const [isLoadingSave, setIsLoadingSave] = useState(false);
 
-  // TODO: clean these up: create a component "CheckboxGroup" that take directly an array of string as a props
-  const [screenTypeOptions, setScreenTypeOptions] = useState<Array<IOptionScreenType>>([]); // For checkboxes
-  // useEffect below: watch screenTypeOptions state changed
-  useEffect(() => {
-    // Whenever screenTypeOptions changed (= checkboxes changed), update roomInput.screenTypeIds
-    const screenTypeIdsSelected = screenTypeOptions
-      .filter(screenTypeOption => {
-        return screenTypeOption.isSelected === true;
-      })
-      .map(screenTypeOption => {
-        return screenTypeOption.screenType.id;
-      });
-    setRoomInput({ ...roomInput, screenTypeIds: screenTypeIdsSelected });
-    // Comment below is there to disable a stupid linter warning
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [screenTypeOptions]);
-
   const onDialogEnter = () => {
     if (!props.roomToEdit) {
       setRoomInput({ name: '', clusterId: '5dc77c3f06e6b13dc44b6cac', screenTypeIds: [], totalRows: 0, totalSeatsPerRow: 0 });
@@ -67,8 +51,6 @@ const DialogAddOrEditRoom: FunctionComponent<IDialogAddOrEditRoomProps> = (props
         totalSeatsPerRow: props.roomToEdit.totalSeatsPerRow,
       });
     }
-    
-    initScreenTypeOptionsValue();
   }
 
   const onDialogClose = () => {
@@ -104,49 +86,18 @@ const DialogAddOrEditRoom: FunctionComponent<IDialogAddOrEditRoomProps> = (props
     }
   }
 
-  const initScreenTypeOptionsValue = () => {
-    const tmpScreenTypeOptions = props.screenTypeList.map(screenType => {
-      let isScreenTypeSelected = false;
-
-      if (!props.roomToEdit) {
-        isScreenTypeSelected = false;
-      } else {
-        // Check if
-        isScreenTypeSelected = props.roomToEdit.screenTypes.map(currentScreenType => currentScreenType.id).includes(screenType.id);
-      }
-
-      return {
-        screenType: screenType,
-        isSelected: isScreenTypeSelected,
-      };
-    })
-    setScreenTypeOptions(tmpScreenTypeOptions);
-  }
-
   const renderScreenTypeCheckboxes = () => {
     return (
       <FormGroup style={{marginLeft: 10, marginBottom: 20,}}>
         <FormLabel>Screen types:</FormLabel>
         <div style={{display: 'flex',}}>
-          {screenTypeOptions.map(screenTypeOption => (
-            <FormControlLabel
-              key={screenTypeOption.screenType.id}
-              control={
-                <Checkbox
-                  checked={screenTypeOption.isSelected}
-                  onChange={(event, checked) => {
-                    const tmpScreenTypeOptions = [...screenTypeOptions];
-                    const foundIndex = tmpScreenTypeOptions.findIndex(x => x.screenType.id === screenTypeOption.screenType.id);
-                    tmpScreenTypeOptions[foundIndex] = {...screenTypeOption, isSelected: checked };
-                    setScreenTypeOptions(tmpScreenTypeOptions);
-                  }}
-                  color="primary"
-                />
-              }
-              label={screenTypeOption.screenType.name}
-              style={{flex: 1}}
-            />
-          ))}
+          <CheckboxGroup
+            options={props.screenTypeList}
+            fieldValue="id"
+            fieldLabel="name"
+            selectedValues={roomInput.screenTypeIds}
+            onChange={(newSelectedValues: any) => { setRoomInput({ ...roomInput, screenTypeIds: newSelectedValues }) }}
+          />
         </div>
       </FormGroup>
     )
