d438f6bdb21431926776dd5bb716e6b01d73cb0c
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-12-24 18:40:53 +0700

    Temporarily fix Home page

--- src/layout/pages/PageDashboard/PageDashboard.tsx
+++ src/layout/pages/PageDashboard/PageDashboard.tsx
@@ -4,9 +4,13 @@ import classes from './PageDashboard.module.scss';
 
 const PageDashboard: FunctionComponent = () => {
   return (
-    <div className={classes['page-content']}>
-      Hello bitches!
+    <div className={classes['page-container']}>
+      <div className={classes['page-content']}>
+        Welcome to Cinex Dashboard!
+      </div>
+      <img className={classes['logo']} src='https://i.imgur.com/xiNxSJa.png' alt='cinex-logo'></img>
     </div>
+
   );
 }
 
